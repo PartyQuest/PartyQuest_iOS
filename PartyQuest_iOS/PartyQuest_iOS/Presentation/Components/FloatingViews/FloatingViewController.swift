eab54830d45a22d78c19b94fa34c01b39081cc9c
ROWAN <kimkj93@naver.com>
2024-03-02 18:12:08 +0900

    feat: FloatingViewController 여러 가지 floatingView를 subview로 가질 수 있도록 제네릭 타입으로 설정

--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Components/FloatingViews/FloatingViewController.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Components/FloatingViews/FloatingViewController.swift
@@ -6,11 +6,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-final class FloatingViewController: UIViewController {
+final class FloatingViewController<FloatingView: FloatingViewType>: UIViewController {
+    private let floatingView: FloatingView
+    
+    private let floatingViewWidthRatio: CGFloat
+    private let floatingViewHeightRatio: CGFloat
+    
+    private var disposeBag: DisposeBag = .init()
+    
+    init(floatingView: FloatingView, widthRatio: CGFloat, heightRatio: CGFloat) {
+        self.floatingView = floatingView
+        self.floatingViewWidthRatio = widthRatio
+        self.floatingViewHeightRatio = heightRatio
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        disposeBag = .init()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setSubviews()
+        setConstraints()
+    }
+    
+    private func setSubviews() {
+        view.addSubview(floatingView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            floatingView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: floatingViewWidthRatio),
+            floatingView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: floatingViewHeightRatio),
+            floatingView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            floatingView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+        ])
     }
 }
