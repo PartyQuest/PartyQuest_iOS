b6bc2edb819c9c1ac1ca6184dfc44d70187f0683
ROWAN <kimkj93@naver.com>
2024-03-02 18:17:15 +0900

    feat: FloatingViewModel 파일 생성

---
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Components/FloatingViews/FloatingViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FloatingViewModel.swift
+//  PartyQuest_iOS
+//
+//  Created by Rowan on 2024/03/02.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class FloatingViewModel {
+    
+}
